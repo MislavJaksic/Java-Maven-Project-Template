e7af9f16befbdbaec3f0962222c6f12322ad2e4e
MislavJaksic <jaksicmislav@gmail.com>
2021-06-23 23:06:40 +0200

    Log correction

--- src/main/java/template/logging/LogLeveler.java
+++ src/main/java/template/logging/LogLeveler.java
@@ -13,7 +13,7 @@ public class LogLeveler {
 		logger.debug("Debug-message");
 		logger.trace("Trace-message");
 
-		logger.info("who={}, what={}/{}, where={}/{}/{}/{}, when={}/{}, why={}, who={}", "1", "2", "3", "4", "5", "6",
+		logger.info("who={}, what={}/{}, where={}/{}/{}/{}, when={}/{}, why={}, how={}", "1", "2", "3", "4", "5", "6",
 				"7", "8", "9", "10", "11");
 	}
 }
